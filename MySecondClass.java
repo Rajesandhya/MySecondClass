7df700ea932a239cae9d061cfa12383867faa131
Rajesh Limbachiya <rajesandhya_143@yahoo.co.in>
2023-11-01 20:21:12 +0000

    MySecondClass

---
+++ MySecondClass.java
@@ -0,0 +1,23 @@
+package mysecondpackage;
+
+public class MySecondClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Who does testing?");
+
+        System.out.println("What are the qualification of tester?");
+
+        System.out.println("Challenges of testers");
+
+        /** Relationship with Developers.
+         * Developing Automation framework from scratch.
+         * Update the test packs when requirements key points changes,
+         * Time constraint : Quality vs Time.
+         */
+
+    }
+
+}
+
+
